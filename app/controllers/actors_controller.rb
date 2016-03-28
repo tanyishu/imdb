b42ee06ada8705ab355d1336076b8124b07a7fc3
tanyishu <kaobei_lah@hotmail.com>
2016-03-28 12:07:27 +0800

    yishu's conflict

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,20 @@
 class ActorsController < ApplicationController
 
+def show
 end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+end
+
+Actor = params [:name][:year_of_birth][:thumbnail]
