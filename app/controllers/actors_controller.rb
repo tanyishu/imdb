d8fbe3538cad0369c1d3acc904bc9f32eeef5b36
Koh Leok Si <LS@Kohs-MacBook-Air.local>
2016-03-28 12:07:03 +0800

    preparing for conflict

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,22 @@
 class ActorsController < ApplicationController
 
+ def show
+ end
+
+ def new
+ end
+
+ def create
+ end
+
+ def edit
+ end
+
+ def update
+ end
+
 end
+
+private
+
+Actor params are :name, :year_of_birth, :thumbnail
