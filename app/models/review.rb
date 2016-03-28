3583c2691a230eada6cae98fafe620d50cd7b39b
Koh Leok Si <LS@Kohs-MacBook-Air.local>
2016-03-28 11:21:28 +0800

    initial commit

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ActiveRecord::Base
+  belongs_to :Movie
+  belongs_to :User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :reviews
+
+  def fullname
+"#{first_name} #{last_name}"
+  end
+
 end
